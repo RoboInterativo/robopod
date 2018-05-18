f630e3e488ddd0ab148cd9df6c6b3b3ff89225bb
chiefexb <chiefexb@github.com>
2018-05-19 02:53:33 +0300

    1st

---
+++ pods/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Author(models.Model):
+    title=models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+
+class Podcast(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    podcast_title=models.CharField(max_length=1000)
+class Podcast_episode (models.Model):
+    episode_title=models.CharField(max_length=200)
+    publicated=models.DateField()
+    pod_description=models.CharField(max_length=1000)
+    podcast_file=models.FileField()
+    podcast = models.ForeignKey(Podcast,on_delete=models.CASCADE)
