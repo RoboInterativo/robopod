90a343b7a118e9ba272633a498a9e57ac50e85be
chiefexb <chief_exb@mail.ru>
2018-05-19 14:48:31 +0300

    1st Pycharm

--- pods/admin.py
+++ pods/admin.py
@@ -4,4 +4,4 @@ from django.contrib import admin
 from .models import *
 admin.site.register(Author)
 admin.site.register(Podcast)
-admin.site.register(Podcast_episode)
+admin.site.register(PodcastEpisode)
