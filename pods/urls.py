c2700181cf312cd679534fd9d822420299b0552a
chiefexb <chief_exb@mail.ru>
2018-05-22 02:32:25 +0300

    1

--- pods/urls.py
+++ pods/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url,include 
 from django.urls import  path,re_path
 from . import views
-from counterdb.views import  counter
+#1
+from pods.views import  *
 urlpatterns = [
     path("", views.index, name='index'),
-   
+
     
     
 ]
