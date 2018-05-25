d49607f1ed65bf4ce3ca131b2399a19b9a2ef3cc
chiefexb <chief_exb@mail.ru>
2018-05-25 21:51:08 +0300

    1

--- pods/models.py
+++ pods/models.py
@@ -8,12 +8,18 @@ class Author (models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return '{} '.format(self.title)
+
 
 class Podcast(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     podcast_title = models.CharField(max_length=1000)
     podcast_description = models.CharField(max_length=1000)
     podcast_image = models.ImageField(fs)
+
+    def __str__(self):
+        return '{} -{}'.format(self.podcast_title,self.podcast_description)
 
 
 class PodcastEpisode(models.Model):
@@ -22,3 +28,6 @@ class PodcastEpisode(models.Model):
     pod_description = models.CharField(max_length=1000)
     podcast_file = models.FileField(fs)
     podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} -{}'.format(self.episode_title,self.pod_description)
